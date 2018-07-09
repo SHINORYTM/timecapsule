06e3a31823b94f05ab01d773842a0350017f6e66
SHINORYTM <st.mmmazerudake@gmail.com>
2018-07-09 21:35:34 +0900

    remake

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'home/top'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/" => "home#top"
-  get "about" => "home#about"
 
+  #home top
+  get '/' => "home#top"
+  get "about" => "home#about"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #users
   get "signup" =>"users#new"
   get "login" => "users#login_form"
+  get 'users/index'
 end
