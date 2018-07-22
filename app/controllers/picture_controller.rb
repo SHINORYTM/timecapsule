f17207a767b8b767aab445bd7d4733c9446a0f83
SHINORYTM <st.mmmazerudake@gmail.com>
2018-07-22 19:10:41 +0900

    remake email

--- app/controllers/picture_controller.rb
+++ app/controllers/picture_controller.rb
@@ -16,7 +16,7 @@ class PictureController < ApplicationController
     if @picture.present?
       MakeMailer.make_email(@user,@picture).deliver
       redirect_to("/users/#{@user.id}")
-      flash[:notice] = "Timecapsuleを送信しました"
+      flash[:notice] = "Timecapsuleで：#{@picture.title}を送信しました"
     end 
   end
 
