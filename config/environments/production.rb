21ca657256563fab252e4e11055f493b2c1c0795
SHINORYTM <st.mmmazerudake@gmail.com>
2018-07-22 21:25:46 +0900

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+	address:               'smtp.gmail.com',
+	port:      	           587,
+	domain:                'example.com',
+	user_name:             'test.timecapsule.0721@gmail.com',
+	password:              'stts4230',
+	authentication:        'plain',
+	enable_starttls_auto:  true
+}
+
+config.action_mailer.default_url_options= { host:'test-timecapsule.herokuapp.com' }
 
 end
