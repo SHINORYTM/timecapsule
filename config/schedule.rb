9648445107946aa552b1abe16cfde6782daeb54f
SHINORYTM <st.mmmazerudake@gmail.com>
2018-07-27 11:56:15 +0900

    create batch

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,8 +5,15 @@
 
 # Example:
 #
-set :output, "/path/to/my/cron_log.log"
-set :environment, :development
+# Rails.rootを使用するために必要
+env :PATH, ENV['PATH']
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+# cronを実行する環境変数
+rails_env = ENV['RAILS_ENV'] || :development
+# cronを実行する環境変数をセット
+set :environment, rails_env
+# cronのログの吐き出し場所
+set :output, "#{Rails.root}/log/cron.log"
 #
 every 1.minutes do
  runner "PictureController.batch"
