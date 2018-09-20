3f30746288a2cf17372697b00ba20135e37dd5c2
SHINORYTM <st.mmmazerudake@gmail.com>
2018-09-20 19:36:22 +0900

    create login page

--- config/initializers/carrieawave.rb
+++ config/initializers/carrieawave.rb
@@ -3,8 +3,8 @@ if Rails.env == 'production'
       config.fog_provider = 'fog/aws'
     config.fog_credentials = {
      provider: 'AWS',
-     aws_access_key_id: ENV['AKIAJ3OFXHG56YUUBKXA'],
-     aws_secret_access_key: ENV['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'],
+     aws_access_key_id: ENV['ACCESS_KEY_ID'],
+     aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
      region: 'ap-northeast-1'
      }
     config.fog_directory = 'timecapsule-image'
