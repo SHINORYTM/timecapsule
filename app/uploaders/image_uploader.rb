f98bfc26c0a3f175fbf99023832315b9d3b550c5
SHINORYTM <st.mmmazerudake@gmail.com>
2018-09-23 11:05:37 +0900

    store

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,7 +15,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
   # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  "images"
+  # "images"
+  ""
   end
 
   
